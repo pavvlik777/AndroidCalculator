7e69075a0e90522788a023f57ae52e70d25bb253
pavvlik777 <41309600+pavvlik777@users.noreply.github.com>
2019-10-29 01:03:02 +0300

    added numbers and basic fragments; changed app icon

---
+++ app/src/main/java/com/example/calculator/NumbersFragment.java
@@ -0,0 +1,36 @@
+package com.example.calculator;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class NumbersFragment extends Fragment {
+    public NumbersFragment() {
+        // Required empty public constructor
+    }
+
+    public static NumbersFragment newInstance() {
+        NumbersFragment fragment = new NumbersFragment();
+        Bundle args = new Bundle();
+
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_numbers, container, false);
+    }
+}
